4541ce0a5a656abe827a50a386efab31b9e17df1
Michael Gill <mgill@metaqueue.net>
2019-03-18 17:14:37 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+require_once "include/etc/session.php";
+require_once "include/view/page/index/indexIncludeFiles.php";
+require_once "include/model/AdminFaqModel.php";
+siteSession();
+redirect("/login/");
+?>
